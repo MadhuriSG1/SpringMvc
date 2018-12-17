5dfb2d351ed32c090ce91e5d24712bfcb6fb6b34
Madhuri Gaikwad <madhuri.gaikwad198@gmail.com>
2018-12-17 19:23:39 +0530

    first commit

---
+++ src/main/java/com/api/services/RegistrationServiceImpl.java
@@ -0,0 +1,16 @@
+package com.api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.api.models.User;
+import com.api.repo.RegRepository;
+//@Service
+public class RegistrationServiceImpl implements RegistationService {
+	@Autowired
+	private RegRepository regRepository;
+	@Override
+	public void save(User user) {
+		regRepository.save(user);
+	}
+
+}
